585aeac64359a7ae1cd135787e1e6bf98b1566f2
SumitKumar55 <sumit.kumar@noble-software.com>
2023-04-19 23:48:21 +0530

    Show loading skeletons

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -12,7 +12,7 @@ interface Props {
 
 function GameCard({ game }: Props) {
 	return (
-		<Card borderRadius={10} overflow="hidden">
+		<Card width="auto" borderRadius={10} overflow="hidden">
 			<Image src={getCroppedImageUrl(game.background_image)} />
 			<CardBody>
 				<Heading fontSize="2xl">{game.name}</Heading>
