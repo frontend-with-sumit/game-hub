fe949e809984b62784f58fe3d3874ad5878fe263
Sumit <sk990005@outlook.com>
2024-01-20 12:39:25 +0530

    handle errors

--- src/routes.tsx
+++ src/routes.tsx
@@ -2,11 +2,13 @@ import { createBrowserRouter } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import Layout from "./pages/Layout";
 import GameDetailPage from "./components/GameDetailPage";
+import ErrorPage from "./components/ErrorPage";
 
 const router = createBrowserRouter([
 	{
 		path: "/",
 		element: <Layout />,
+		errorElement: <ErrorPage />,
 		children: [
 			{ index: true, element: <HomePage /> },
 			{ path: "games/:id", element: <GameDetailPage /> },
