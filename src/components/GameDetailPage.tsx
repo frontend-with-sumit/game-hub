09c308b3f49dcc0eb06bfd066413af51ab5ffa38
Sumit <sk990005@outlook.com>
2024-01-21 14:48:28 +0530

    fix the navbar

--- src/components/GameDetailPage.tsx
+++ src/components/GameDetailPage.tsx
@@ -1,10 +1,10 @@
-import { Heading, Spinner } from "@chakra-ui/react";
+import { GridItem, Heading, SimpleGrid, Spinner } from "@chakra-ui/react";
 import { useParams } from "react-router-dom";
 import useGame from "../hooks/useGame";
 import ExpandableText from "./ExpandableText";
 import GameAttributes from "./GameAttributes";
-import GameTrailer from "./GameTrailer";
 import GameScreenshots from "./GameScreenshots";
+import GameTrailer from "./GameTrailer";
 
 const GameDetailPage = () => {
 	const { slug } = useParams();
@@ -15,13 +15,17 @@ const GameDetailPage = () => {
 	if (error || !game) throw error;
 
 	return (
-		<>
-			<Heading>{game.name}</Heading>
-			<ExpandableText>{game.description_raw}</ExpandableText>
-			<GameAttributes game={game} />
-			<GameTrailer gameId={game?.id} />
-			<GameScreenshots gameId={game?.id} />
-		</>
+		<SimpleGrid columns={{ base: 1, md: 2 }} spacing={5}>
+			<GridItem>
+				<Heading>{game.name}</Heading>
+				<ExpandableText>{game.description_raw}</ExpandableText>
+				<GameAttributes game={game} />
+			</GridItem>
+			<GridItem>
+				<GameTrailer gameId={game?.id} />
+				<GameScreenshots gameId={game?.id} />
+			</GridItem>
+		</SimpleGrid>
 	);
 };
 
